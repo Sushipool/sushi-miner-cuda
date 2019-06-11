fbeb370e15b87adedcbbc4a29eebc8a2eb993f49
tomkha <mpiskunov@gmail.com>
2019-06-11 14:08:56 +0300

    Always show 3 significant digits of the hashrate.

--- src/Utils.js
+++ src/Utils.js
@@ -2,18 +2,15 @@ const fs = require('fs');
 const JSON5 = require('json5');
 const Nimiq = require('@nimiq/core');
 
-exports.humanHashrate = function (hashes, decimals = 1) {
+exports.humanHashrate = function (hashes) {
     let thresh = 1000;
-    if (Math.abs(hashes) < thresh) {
-        return hashes + ' H/s';
-    }
-    let units = ['kH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s', 'EH/s', 'ZH/s', 'YH/s'];
-    let u = -1;
-    do {
+    let units = ['H/s', 'kH/s', 'MH/s', 'GH/s', 'TH/s', 'PH/s', 'EH/s', 'ZH/s', 'YH/s'];
+    let u = 0;
+    while (Math.abs(hashes) >= thresh && u < units.length - 1) {
         hashes /= thresh;
         ++u;
-    } while (Math.abs(hashes) >= thresh && u < units.length - 1);
-    return hashes.toFixed(decimals) + ' ' + units[u];
+    }
+    return hashes.toFixed(hashes >= 100 ? 0 : hashes >= 10 ? 1 : 2) + ' ' + units[u];
 }
 
 exports.readConfigFile = function (fileName) {
