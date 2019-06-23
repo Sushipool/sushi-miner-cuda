79e1644c4fdee11f11bffe869412530b400b1b43
tomkha <mpiskunov@gmail.com>
2019-06-23 17:56:31 +0300

    Increase UV_THREADPOOL_SIZE to 32.

--- start_gpu.bat
+++ start_gpu.bat
@@ -1,6 +1,6 @@
 @echo off
 echo "------------------------START Miner----------------------"
-SET UV_THREADPOOL_SIZE=24 
+SET UV_THREADPOOL_SIZE=32
 sushi-miner-cuda.exe
 echo "------------------------END Miner----------------------"
 echo "Something went wrong or you exited"
